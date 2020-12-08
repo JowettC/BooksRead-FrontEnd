569a915d50c6f2d3c8cdf96ad6938e17bbd58007
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-08 09:11:36 +0800

    Login

---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class = "card login__form">
+        <div class ="login__fields">
+            <div class ="login__title"><h1>Books Read Login</h1></div>
+            <br/>
+        <b-field label="Username"
+            :label-position="labelPosition"
+            :type="username_type"
+            :message="username_msg">
+            <b-input value="johnsilver" maxlength="30"></b-input>
+        </b-field>
+
+        <b-field label="Password"
+            :label-position="labelPosition"
+            :type="password_type"
+            :message="password_msg">
+            <b-input value="123" type="password" maxlength="30"></b-input>
+        </b-field>
+        <b-button type="is-primary" outlined>Outlined</b-button>
+        </div>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+
+
+export default {
+    data() {
+        return {
+            labelPosition: 'on-border',
+            username_type:"",
+            username_msg:"",
+            password_type:"",
+            password_msg:"",
+        }
+
+    },
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style>
+.login__form{
+    width: 500px;
+    margin:auto;
+}
+.login__fields{
+    padding: 50px;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="Login">
-
+      <login-form/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import LoginForm from '@/components/login/LoginForm.vue'
 
 export default {
   name: 'Home',
   components: {
+      LoginForm
   }
 }
 </script>
