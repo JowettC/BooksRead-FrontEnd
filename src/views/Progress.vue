08eb32516baf309952f9328315e35699640ca818
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-10 21:53:21 +0800

    chart

---
+++ src/views/Progress.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+<progress-bar-chart/>
+    </div>
+</template>
+<script>
+import ProgressBarChart from '@component/charts/ProgressBarChart'
+export default {
+  name: 'Progress',
+  components: {
+      ProgressBarChart
+  }
+}
+</script>
