18c12bc66791ae973811803f7eac64b0123421a1
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-10 21:53:47 +0800

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
+    <nav-bar v-if="$store.state.token" />
     <router-view/>
   </div>
 </template>
-
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
