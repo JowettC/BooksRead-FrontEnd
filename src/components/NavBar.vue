6c18edd7a3b915d5275ff3406207c1d746a0d53f
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-10 21:53:54 +0800

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,46 @@
 <template>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                <router-link to="/">Home</router-link>
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                <router-link to="/Progress">Your Progress</router-link>
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary"  @click="logout">
+                        <strong>Log Out</strong>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
 </template>
+<script>
+export default {
+  name: "nav-bar",
+  methods: {
+    async logout() {
+      await this.$store.dispatch("logout");
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+<style scoped>
+.is-primary{
+  margin:auto;
+}
+</style>
