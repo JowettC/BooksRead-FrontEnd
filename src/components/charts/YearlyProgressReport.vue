ff91a9b03c774197c39d525b0d55077af3424756
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-11 01:08:32 +0800

    Create YearlyProgressReport.vue

---
+++ src/components/charts/YearlyProgressReport.vue
@@ -0,0 +1,27 @@
+<script>
+import { Line,mixins } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+  props: ['chartData', 'options'],
+  mounted() {
+    this.renderChart(
+      this.chartData,
+      {
+          scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      }
+    );
+  }
+};
+</script>
