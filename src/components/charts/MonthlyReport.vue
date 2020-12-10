88b96a8a5d9b623529a80e5929bf8a36757ccddc
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-11 01:08:12 +0800

    monthly report

---
+++ src/components/charts/MonthlyReport.vue
@@ -0,0 +1,42 @@
+<script>
+import { Bar,mixins  } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveProp],
+  props: ['chartData', 'options'],
+  
+  mounted () {
+    this.renderChart(this.chartData, {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },)
+  }
+  // mounted() {
+  //   this.renderChart(
+  //     // chartData
+  //     {
+  //       labels: this.chartData.dataLabel,
+  //       datasets: [
+  //         {
+  //           label: "Data One",
+  //           backgroundColor: "#f87979",
+  //           data: this.chartData.dataSet
+  //         }
+  //       ]
+  //     },
+
+  //     { responsive: true, maintainAspectRatio: false }
+  //   );
+  // }
+};
+</script>
