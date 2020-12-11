b673e517510de997731b2146bde26c1f283184cb
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-11 10:02:14 +0800

    change navbar logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,8 @@
 <template>
     <b-navbar>
         <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
+            <b-navbar-item >
+                <b>BOOOKIE</b>
             </b-navbar-item>
         </template>
         <template slot="start">
